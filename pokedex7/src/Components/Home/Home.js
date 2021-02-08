822d5357349a8ddf0766ec170d765c293c01a84e
DanielRatti <danielpr55rr@gmai.com>
2021-02-08 15:12:22 -0300

    Criado os componentes / rotas

---
+++ pokedex7/src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../Header/Header"
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <p>Home</p>
+        </div>
+    )
+}
+
+export default HomePage
