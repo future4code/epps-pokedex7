42a5b9b5ce62095ce3b756381e0754f3f02af9a0
Fernando <fernando.loures@hotmail.com>
2021-02-09 16:44:05 -0300

    primeira formatação da página pokedex

--- pokedex/src/Components/Pokedex/Pokedex.js
+++ pokedex/src/Components/Pokedex/Pokedex.js
@@ -1,37 +1,44 @@
-import Header from "../Header/Header"
-import axios from 'axios'
-import React, { useState, useEffect } from "react";
-
+import React from "react";
+import "nes.css/css/nes.min.css";
+import {Card, ContainerPokedex, Tittle, Img, BtnDiv, I} from './PokedexStyled'
 
 const PokedexPage = () => {
 
-    const [poke, setpoke] = useState([])
-
-    const getPoke = () =>{
-        axios.get("https://pokeapi.co/api/v2/pokemon")
-        .then((respondeu)=>{
-            setpoke(respondeu.data.results)
-            console.log(respondeu.data.results)
-        }).catch((error)=>{
-            alert('nao pegou os pokemons')
-        })
+    let poke = ([{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+    { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
+    { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
+    { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" }])
+    
+    const pokemonDelete = () =>{
+        console.log('deletar da lista')
     }
 
-    useEffect(()=>{
-        getPoke()
-    }, [])
+    const pokemonDetails = () =>{
+        console.log('Ir para detalhes')
+    }
 
-    
     return (
         <div>
-            <p>Pokedex - Lista de todos os pokemons</p>
-            {poke.map((t)=>{
-          return(
-              <div>
-                    <p>{t.name}</p>
-                </div>
-          )
-      })}
+            <Tittle className="nes-text is-primary">
+            Pokedex </Tittle>
+            <ContainerPokedex className="nes-container is-centered">
+            {poke.map(pokemon => {
+                return (
+                    <Card key={pokemon.name} className="nes-container is-centered">
+                        {(<Img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"} />) ? 
+                        <Img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"} /> :
+                    <p>Carregando imagem</p> }
+                        <div className="nes-text is-primary">
+                            Name: {pokemon.name}
+                        </div>
+                        <BtnDiv>
+                            <button onClick={pokemonDetails} type="button" className="nes-btn is-primary">Detalhes</button>
+                            <button onClick={pokemonDelete} type="button" className="nes-btn is-warning">Warning</button>
+                        </BtnDiv>
+                    </Card>
+                )
+            })}
+            </ContainerPokedex>
         </div>
     )
 }
