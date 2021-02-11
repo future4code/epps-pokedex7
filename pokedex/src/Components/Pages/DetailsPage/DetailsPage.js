8f4cda03179b74a066a921b71fcdb242c455ff8a
DanielRatti <danielpr55rr@gmai.com>
2021-02-11 16:27:59 -0300

    feito pagina de detalhes

--- pokedex/src/Components/Pages/DetailsPage/DetailsPage.js
+++ pokedex/src/Components/Pages/DetailsPage/DetailsPage.js
@@ -1,21 +1,74 @@
-import React from "react"
-import Header from "../../Header/Header"
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { BASE_URL } from "../../Bases/urls";
+import { useRequestData } from "../../Hooks/useRequestData";
+
+const DetailsContainer = styled.div``;
+
+const ImgContainer = styled.div``;
+
+const TypeContainer = styled.div``;
+
+const StatsContainer = styled.div``;
+
+const MovesContainer = styled.div``;
+
+const InfosContainer = styled.div``;
 
 const DetailsPage = () => {
-    return (
-        <div>
-            <p>Details</p>
-            <p>
-            Poderes
-            hp: 60
-            attack: 62
-            defense: 63
-            special-attack: 80
-            special-defense: 80
-            speed: 60
-            </p>
-        </div>
-    )
-}
+  const pathParams = useParams();
+  const getDetails = useRequestData(`${BASE_URL}/${pathParams.id}`, undefined);
 
-export default DetailsPage
+
+
+  return (
+    <div>
+      <DetailsContainer>
+        {getDetails && (
+          <ImgContainer>
+            <img src={getDetails.sprites.front_default}/>
+            <img src={getDetails.sprites.back_default}  />
+          </ImgContainer>
+        )}
+
+        <InfosContainer>
+          <TypeContainer>
+            <h3>Type</h3>
+            {getDetails &&
+              getDetails.types.map((type) => {
+                return (
+                  <div>
+                    <p>{type.type.name}</p>
+                  </div>
+                );
+              })}
+          </TypeContainer>
+
+          <StatsContainer>
+            <h3>Stats</h3>
+            {getDetails &&
+              getDetails.stats.map((stat) => {
+                return (
+                  <div>
+                    <p>
+                      <strong>{stat.stat.name}:</strong> {stat.base_stat}
+                    </p>
+                  </div>
+                );
+              })}
+          </StatsContainer>
+
+          <MovesContainer>
+            <h3>Moves</h3>
+            {getDetails &&
+              getDetails.moves.map((move, num) => {
+                return num < 5 && <p>{move.move.name}</p>;
+              })}
+          </MovesContainer>
+        </InfosContainer>
+      </DetailsContainer>
+    </div>
+  );
+};
+
+export default DetailsPage;
