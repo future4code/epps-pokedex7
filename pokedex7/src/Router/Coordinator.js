44d2c90cbbb21f2af5365efdf96d827200fe48f0
carolaine-viana <carolsantos14@hotmail.com>
2021-02-11 13:18:58 -0300

    test

---
+++ pokedex7/src/Router/Coordinator.js
@@ -0,0 +1,11 @@
+export const goToHomePage = (history) => {
+    history.push("/");
+  };
+
+  export const goToPokedex = (history) => {
+    history.push("/pokedex");
+  };
+
+  export const goToPokemonDetails = (history) => {
+    history.push("/details-page");
+  };
