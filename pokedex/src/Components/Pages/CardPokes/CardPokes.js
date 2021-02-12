d5064f635f0b6ba23610bd368ba76bbe9f9a531c
Fernando <fernando.loures@hotmail.com>
2021-02-12 06:44:04 -0300

    Agora botão de deletar aparece apenas na pokedex

--- pokedex/src/Components/Pages/CardPokes/CardPokes.js
+++ pokedex/src/Components/Pages/CardPokes/CardPokes.js
@@ -33,9 +33,9 @@ const CardPokes = (props) => {
                 <img src={addPoke.sprites.front_default}/>
                 <p>{addPoke.name}</p> 
             <div>
-                <button type="button" class="nes-btn is-success" onClick={props.addPoke}>Me escolha</button>
+                {!states.pokedexPage && <button type="button" class="nes-btn is-success" onClick={props.addPoke}>Me escolha</button>}
                 <button type="button" class="nes-btn is-primary" onClick={() => goToDetailsPokemon(history, addPoke.id)}>Mais sobre mim</button>
-                <button class="nes-btn is-error" onClick={() => removePoke(props.name)}>Deletar</button>
+                {states.pokedexPage && <button class="nes-btn is-error" onClick={() => removePoke(props.name)}>Deletar</button>}
             </div>
             </div>
             }
